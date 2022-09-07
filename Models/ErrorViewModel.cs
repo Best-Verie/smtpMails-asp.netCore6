13f2fc4bf781805e8d2c54fbb1c3429894fc12bd
Best-Verie <irabestverie2016@gmail.com>
2022-09-07 15:17:03 +0200

    project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace smtp_sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
